46ed0590cbe0d2141f557a9b907b53fc4876cbc8
trc2573 <trc2573@gmail.com>
2020-07-28 16:35:11 +0530

    a change

--- gittest2/src/org/trc/FirstGit.java
+++ gittest2/src/org/trc/FirstGit.java
@@ -4,7 +4,7 @@ public class FirstGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("FIRST GIT");
+		System.out.println("FIRST GIT change 1");
 
 	}
 
